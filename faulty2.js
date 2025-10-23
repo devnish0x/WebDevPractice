39fcb89f7025807a83a2bb3f37722e2d532cdf20
Nishan <nishanmaindan43@gmail.com>
2025-10-23 22:19:36 +0530

    HarryCode

---
+++ faulty2.js
@@ -0,0 +1,23 @@
+let a = prompt("Enter first number:");
+let b = prompt("Enter second number:")
+let c = prompt("Enter operator (+, -, *, /):")
+ 
+let r = Math.random()
+console.log(r)
+
+let x =
+{
+    "+":"-",
+    "-":"/",
+    "*":"+",
+    "/":"**"
+}   
+
+if (r<0.1) {
+    c = x(c)
+    console.log(`${a} ${c} ${b} = ${eval(a+c+b)}`) 
+    
+}
+else {
+    console.log(`${a} ${c} ${b} = ${eval(a+c+b)}`) 
+}
