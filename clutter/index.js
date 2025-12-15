c8c1b04b1cb86f56fcd472b7d1e731bd03972a31
Nishan <nishanmaindan43@gmail.com>
2025-12-15 19:19:57 +0530

    ClutterExerecis15

---
+++ clutter/index.js
@@ -0,0 +1,27 @@
+import fs from "fs/promises"
+import fss from "fs"
+import path from "path"
+
+let dirr = "C:\\Users\\nisha\\webdev\\clutter"
+
+const files = await fs.readdir(dirr)
+console.log(files)
+
+for (const file of files) {
+    const e = path.extname(file)
+    const ext = e.slice(1)
+    console.log(ext)
+
+    if (ext !="json" && ext != "js"){
+        if(fss.existsSync(path.join(dirr,ext))){
+            fs.rename(path.join(dirr,file),path.join(dirr,ext,file))
+            console.log("yes")
+        }
+        else{
+            fs.mkdir(path.join(dirr,ext))
+            fs.rename(path.join(dirr,file),path.join(dirr,ext,file))
+            console.log("no")
+        }
+
+    }
+}
